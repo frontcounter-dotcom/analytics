fcc928f52c5629c667eb97945d07992261031f54
Brio Manager <briomanager@Brios-MacBook-Air.local>
2026-05-22 10:52:18 -0700

    Initial draft untried.

---
+++ scripts/custie_info_audit.py
@@ -0,0 +1,96 @@
+# conda env info_audit_py3.13:
+# numpy==2.4.4
+# pandas==3.0.2
+
+from typing import Final, List
+from datetime import datetime
+import logging
+
+import numpy as np
+import pandas as pd
+
+class Columns:
+    ADDRESS = "Address"
+    EMAIL = "Email"
+    NAME = "Name"
+    PHONE = "Phone"
+    SECONDARY_TEL = "Secondary Tel"
+    TOTAL_ORDERS = "Total orders"
+
+CUSTIE_CSV_FP: Final[str] = "~/Downloads/CC-Customers_2019-11-26-2026-05-21.csv"
+CONTACT_COLS: Final[List[str]] = [Columns.EMAIL, Columns.PHONE, Columns.ADDRESS]
+CONTACT_COLS_ALL: Final[List[str]] = CONTACT_COLS + [Columns.SECONDARY_TEL]
+OUTPUT_DIR: Final[str] = "~/analytics/customer_info"
+
+def main() -> None:
+
+    custie_df = pd.read_csv(CUSTIE_CSV_FP)
+
+    custie_df = clean(df=custie_df)
+
+    custie_df = find_and_delete_invalid(df=custie_df)
+    breakpoint()
+    custie_df = custie_df[
+        ~(custie_df[CONTACT_COLS_ALL].isna().all(axis=1) &
+        (custie_df[Columns.TOTAL_ORDERS] > 0))
+    ]
+    breakpoint()
+    missing_contact_df = custie_df[custie_df[CONTACT_COLS].isna().any(axis=1)]
+    breakpoint()
+    to_email_df = missing_contact_df[missing_contact_df[Columns.EMAIL].notna()]
+    breakpoint()
+    to_call_df = missing_contact_df[
+        missing_contact_df[Columns.PHONE].notna() |
+        missing_contact_df[Columns.SECONDARY_TEL].notna()
+    ]
+
+    save_output(to_email_df=to_email_df, to_call_df=to_call_df)
+
+    return
+
+
+
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    """Clean up dataframe and set types."""
+    df = df.astype(
+        dtype={
+            Columns.ADDRESS: str,
+            Columns.EMAIL: str,
+            Columns.NAME: str,
+            Columns.PHONE: str,
+            Columns.SECONDARY_TEL: str,
+            Columns.TOTAL_ORDERS: int
+        }
+    )
+
+    df = df.replace('', np.nan)
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+
+    df[Columns.TOTAL_ORDERS] = df[Columns.TOTAL_ORDERS].fillna(0)
+
+    return df
+
+
+def find_and_delete_invalid(df: pd.DataFrame) -> pd.DataFrame:
+    """Validate contact fields, and delete invalid contact info."""
+    ...
+    return df
+
+
+def save_output(to_email_df: pd.DataFrame, to_call_df: pd.DataFrame) -> None:
+    """Save outputs."""
+    today = str(datetime.today())
+    to_email_fp = f"{OUTPUT_DIR}/to_email_{today}.csv"
+    to_call_fp = f"{OUTPUT_DIR}/to_call_{today}.csv"
+
+    logging.info(f"Saving to_email_df to {to_email_fp}")
+    to_email_df.to_csv(to_email_fp)
+
+    logging.info(f"Saving to_call_df to {to_call_fp}")
+    to_call_df.to_csv(to_call_fp)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
