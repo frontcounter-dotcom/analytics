4041dea69937ce04ed4b65b1b0ea6cf6430cc7d7
Brio Manager <briomanager@Brios-MacBook-Air.local>
2026-05-27 13:45:20 -0700

    Add find_and_delete_invalid_phone_nos.

--- scripts/custie_info_audit.py
+++ scripts/custie_info_audit.py
@@ -2,6 +2,7 @@
 # email_validator==2.3.0
 # numpy==2.4.4
 # pandas==3.0.2
+# phonenumbers==9.0.18
 # typeguard==4.5.1
 
 import logging
@@ -11,6 +12,7 @@ from datetime import datetime
 import email_validator
 import numpy as np
 import pandas as pd
+import phonenumbers
 from typeguard import typechecked
 
 
@@ -31,6 +33,7 @@ CUSTIE_CSV_FP: Final[str] = "~/Downloads/CC-Customers_2019-11-26-2026-05-21.csv"
 CONTACT_COLS: Final[List[str]] = [Columns.EMAIL, Columns.PHONE, Columns.ADDRESS]
 CONTACT_COLS_ALL: Final[List[str]] = CONTACT_COLS + [Columns.SECONDARY_TEL]
 OUTPUT_DIR: Final[str] = "~/analytics/customer_info"
+TODAY = str(datetime.today())
 
 
 @typechecked
@@ -95,13 +98,14 @@ def find_and_delete_invalid(df: pd.DataFrame) -> pd.DataFrame:
     """Validate contact fields, and delete invalid contact info."""
     df = find_and_delete_invalid_emails(df=df)
 
-    ...
+    df = find_and_delete_invalid_phone_nos(df=df, phone_col=Columns.PHONE)
+    df = find_and_delete_invalid_phone_nos(df=df, phone_col=Columns.SECONDARY_TEL)
     
     return df
 
 @typechecked
 def find_and_delete_invalid_emails(df: pd.DataFrame) -> pd.DataFrame:
-    """Validate contact fields, and delete invalid contact info."""
+    """Format and validate email, and delete invalid."""
     normalized_emails = []
     for email in df[Columns.EMAIL]:
         email_validated = np.nan
@@ -129,12 +133,76 @@ def find_and_delete_invalid_emails(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+# Borrowed and modified from https://github.com/crickets-and-comb/bfb_delivery
+@typechecked
+def find_and_delete_invalid_phone_nos(df: pd.DataFrame, phone_col: str) -> pd.DataFrame:
+    """Format and validate phone numbers, and delete invalid."""
+    df[phone_col] = df[phone_col].astype(str).str.strip()
+    df[phone_col] = df[phone_col].apply(lambda x: '' if (x is np.nan or x is None) else x)
+    df[phone_col] = df[phone_col].apply(lambda x: x[:-2] if x.endswith(".0") else x)
+
+    formatting_df = df.copy()
+    formatting_df["formatted_numbers"] = formatting_df[phone_col].apply(
+        lambda number: "+1" + number if (len(number) > 0 and number[0] != "+") else number
+    )
+    
+    parsed_nos = []
+    for number in formatting_df['formatted_numbers']:
+        parsed_no = number
+        if len(number) > 0:
+            try:
+                parsed_no = phonenumbers.parse(number)
+            except Exception as e:
+                parsed_no = ""
+                logger.warning(f"Unable to parse phone number: {number}")
+        parsed_nos.append(parsed_no)
+
+    formatting_df["formatted_numbers"] = parsed_nos
+
+    formatting_df["is_valid"] = formatting_df["formatted_numbers"].apply(
+        lambda number: (
+            phonenumbers.is_valid_number(number)
+            if isinstance(number, phonenumbers.phonenumber.PhoneNumber)
+            else True
+        )
+    )
+    if not formatting_df["is_valid"].all():
+        invalid_numbers = formatting_df[~formatting_df["is_valid"]]
+        logger.warning(
+            f"Invalid phone numbers found in {phone_col}, deleting:\n{invalid_numbers[phone_col].to_list()}"
+        )
+        invalid_numbers_fp = f"{OUTPUT_DIR}/invalid_numbers_{phone_col}_{TODAY}.csv"
+        logging.info(f"Saving invalid numbers for {phone_col} to {invalid_numbers_fp}")
+        invalid_numbers.to_csv(invalid_numbers_fp)
+
+    def _nan_invalids(row):
+        return np.nan if not row["is_valid"] else row["formatted_numbers"]
+    
+    formatting_df["formatted_numbers"] = formatting_df.apply(_nan_invalids, axis=1)
+
+    formatting_df["formatted_numbers"] = [
+        (
+            str(
+                phonenumbers.format_number(
+                    number, num_format=phonenumbers.PhoneNumberFormat.INTERNATIONAL
+                )
+            )
+            if isinstance(number, phonenumbers.phonenumber.PhoneNumber)
+            else number
+        )
+        for number in formatting_df["formatted_numbers"].to_list()
+    ]
+
+    df[phone_col] = formatting_df["formatted_numbers"]
+
+    return df
+
+
 @typechecked
 def save_output(to_email_df: pd.DataFrame, to_call_df: pd.DataFrame) -> None:
     """Save outputs."""
-    today = str(datetime.today())
-    to_email_fp = f"{OUTPUT_DIR}/to_email_{today}.csv"
-    to_call_fp = f"{OUTPUT_DIR}/to_call_{today}.csv"
+    to_email_fp = f"{OUTPUT_DIR}/to_email_{TODAY}.csv"
+    to_call_fp = f"{OUTPUT_DIR}/to_call_{TODAY}.csv"
 
     logging.info(f"Saving to_email_df to {to_email_fp}")
     to_email_df.to_csv(to_email_fp)
